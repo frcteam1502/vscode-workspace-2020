3634bb7d5215c88fc132d7d14fe9f62f9c2080e3
frcteam1502 <frcteam1502@gmail.com>
2020-01-21 19:06:37 -0500

    Stop making me do these commit messages

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,29 @@
 
 package frc.robot;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static class Motors {
+        public static final CANSparkMax SPINNER_LIFT = new CANSparkMax(0, MotorType.kBrushed);
+        public static final CANSparkMax SPIN_WHEEL = new CANSparkMax(1, MotorType.kBrushless);
+    }
+
+    public static class Sensors {
+        public static final DigitalInput SPINNER_LIMIT = new DigitalInput(0);
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -0,0 +1,58 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Spinner extends SubsystemBase {
+
+  CANSparkMax spinWheel;
+  CANSparkMax spinLift;
+  DigitalInput liftLimit;
+  int colorCount = 0;
+  double wheelSpeed = 1;
+
+  public Spinner(CANSparkMax spinLift, CANSparkMax spinWheel, DigitalInput liftLimit) {
+    this.spinLift = spinLift;
+    this.spinWheel = spinWheel;
+    this.liftLimit = liftLimit;
+  }
+  /*
+   * public void SpinLift(double placeholder1) { spinLift.set(placeholder1); }
+   * 
+   * public void SpinWheel(double placeholder2) { spinWheel.set(placeholder2); }
+   */
+
+  public void run() {
+    /*
+     * while(!liftLimit.get()) spinLift.set(1); spinLift.set(0); //TODO Make it so
+     * that it only adds 1 to colorCount every time it is on said color.
+     * while(colorCount < 6) { spinWheel.set(wheelSpeed); if(checkColor()) {
+     * colorCount++; wheelSpeed = wheelSpeed - .15; } } spinWheel.set(0);
+     */
+    CANEncoder enc = spinWheel.getEncoder();
+    if (enc.getPosition() < 10000)
+      spinWheel.set(1);
+    else
+      spinWheel.set(0);
+  }
+
+  // TODO Add to chechColor so it actually checks color.
+  public boolean checkColor() {
+    return true;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
