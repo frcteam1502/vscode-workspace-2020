fb9b5caf5e08f874fa79c181713182d028f3d6d7
frcteam1502 <frcteam1502@gmail.com>
2020-01-23 19:01:27 -0500

    Add gyro sensor test

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,13 +7,52 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.I2C.Port;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
+import edu.wpi.first.wpilibj2.command.button.Button;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+import static com.revrobotics.CANSparkMaxLowLevel.MotorType.*;
+
+import com.revrobotics.CANSparkMax;
+
 /**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants.  This class should not be used for any other purpose.  All constants should be
- * declared globally (i.e. public static).  Do not put anything functional in this class.
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
  *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
+ * <p>
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
  */
 public final class Constants {
+
+    public static final class Sensors {
+        public static final ADXL345_I2C gyro = new ADXL345_I2C(Port.kOnboard, Range.k4G);
+    }
+
+    public static final class Motors {
+        public static final CANSparkMax RANDOM_MOTOR = new CANSparkMax(1, kBrushless);
+    }
+
+    public static final class Joysticks {
+        public static final XboxController XBOX = new XboxController(2);
+        public static final Joystick LEFT_JOYSTICK = new Joystick(0);
+        public static final Joystick RIGHT_JOYSTICK = new Joystick(1);
+    }
+
+    public static final class Buttons {
+        public static final Button A = new JoystickButton(Joysticks.XBOX, 0);
+        public static final Button B = new JoystickButton(Joysticks.XBOX, 1);
+        public static final Button X = new JoystickButton(Joysticks.XBOX, 2);
+        public static final Button Y = new JoystickButton(Joysticks.XBOX, 3);
+        public static final Button LB = new JoystickButton(Joysticks.XBOX, 4);
+        public static final Button RB = new JoystickButton(Joysticks.XBOX, 5);
+        public static final Button START = new JoystickButton(Joysticks.XBOX, 6);
+        public static final Button BACK = new JoystickButton(Joysticks.XBOX, 7);
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/GyroSensor.java
@@ -0,0 +1,42 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.ADXL345_I2C;
+import edu.wpi.first.wpilibj.ADXL345_SPI;
+import edu.wpi.first.wpilibj.I2C;
+import edu.wpi.first.wpilibj.ADXL345_I2C.Axes;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer.Range;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.GyroPostionGetting;
+
+public class GyroSensor extends SubsystemBase {
+  /**
+   * Creates a new GyroSensor.
+   */
+  ADXL345_I2C gyro = new ADXL345_I2C(I2C.Port.kOnboard, Range.k4G);
+
+  public GyroSensor(GyroPostionGetting m_GyroPostionGetting) {
+
+  }
+
+  public void putToSmartDashboard() {
+    SmartDashboard.putNumber("Gyro X", gyro.getX());
+    SmartDashboard.putNumber("Gyro Y", gyro.getY());
+    SmartDashboard.putNumber("Gyro Z", gyro.getZ());
+    SmartDashboard.putNumber("X Axis Acceleration", gyro.getAcceleration(Axes.kX));
+    SmartDashboard.putNumber("Y Axis Acceleration", gyro.getAcceleration(Axes.kY));
+    SmartDashboard.putNumber("Z Axis Acceleration", gyro.getAcceleration(Axes.kZ));
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
