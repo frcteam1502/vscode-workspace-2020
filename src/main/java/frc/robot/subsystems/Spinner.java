fda9c4747b79a06937e61d79b3953500f225edcd
frcteam1502 <frcteam1502@gmail.com>
2020-01-25 16:57:58 -0500

    Extended private color into Color from ColorSensor

--- src/main/java/frc/robot/subsystems/Spinner.java
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -9,8 +9,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.ColorSensorV3;
-import com.revrobotics.ColorSensorV3.RawColor;
 
+import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
@@ -22,14 +22,14 @@ public class Spinner extends SubsystemBase {
   private final ColorSensorV3 colorSensor;
   private int counter = 0;
 
-  private Color startColor;
-  private Color lastColor;
+  private ColorHelper startColor;
+  private ColorHelper lastColor;
   // red green blue yellow. Need to fix later
-  private final Color RED = new Color(.602539, .300049, .097412);
-  private final Color GREEN = new Color(.421875, .472656, .105469);
-  private final Color BLUE = new Color(.234863, .528564, .236572);
-  private final Color YELLOW = new Color(.184814, .422119, .393066);
-  private final Color[] COLOR_MAP = { YELLOW, RED, GREEN, BLUE };
+  private final ColorHelper RED = new ColorHelper(.602539, .300049, .097412);
+  private final ColorHelper GREEN = new ColorHelper(.421875, .472656, .105469);
+  private final ColorHelper BLUE = new ColorHelper(.234863, .528564, .236572);
+  private final ColorHelper YELLOW = new ColorHelper(.184814, .422119, .393066);
+  private final ColorHelper[] COLOR_MAP = { YELLOW, RED, GREEN, BLUE };
 
   public Spinner(ColorSensorV3 colorSensor, CANSparkMax spinner) {
     this.spinner = spinner;
@@ -40,11 +40,12 @@ public class Spinner extends SubsystemBase {
    * Previous color class didnt allow me to easily make new Colors, so i ditched
    * it
    */
-  private class Color {
+  private class ColorHelper extends Color {
 
     private double[] RGB = new double[3];
 
-    public Color(RawColor color) {
+    public ColorHelper(Color color) {
+      super(color.red, color.green, color.blue);
       double mag = color.red + color.green + color.blue;
       double[] RGB = { color.red / mag, color.green / mag, color.blue / mag };
       this.RGB = RGB;
@@ -53,32 +54,27 @@ public class Spinner extends SubsystemBase {
     /**
      * @param RGB must have length of 3
      */
-    public Color(double... RGB) {
-      if (RGB.length == 3)
-        this.RGB = RGB;
-      else
-        throw new IllegalArgumentException();
+    public ColorHelper(double... RGB) {
+      super(RGB[0], RGB[1], RGB[2]);
+      this.RGB = RGB;
     }
 
     /**
      * @param compare compared either directly based on their respected RGB values,
      *                or their proximity to each other via
-     *                {@link #getDifference(Color)}
+     *                {@link #getDifference(ColorHelper)}
      * @return whether or not the two colors are equal
      */
-    public boolean compareTo(Color compare) {
+    public boolean compareTo(ColorHelper compare) {
       return this.RGB == compare.RGB || getDifference(compare) < .1;
     }
 
     /**
-     * Returns the closest color from COLOR_MAP
-     * 
-     * @param color
-     * @return
+     * @return Returns the closest color from COLOR_MAP
      */
-    public Color getClosestColor() {
-      Color closestColor = null;
-      for (Color wheelColor : COLOR_MAP) {
+    public ColorHelper getClosestColor() {
+      ColorHelper closestColor = null;
+      for (ColorHelper wheelColor : COLOR_MAP) {
         if (closestColor == null || wheelColor.getDifference(this) < closestColor.getDifference(this)) {
           closestColor = wheelColor;
         }
@@ -86,7 +82,7 @@ public class Spinner extends SubsystemBase {
       return closestColor;
     }
 
-    private double getDifference(Color compare) {
+    private double getDifference(ColorHelper compare) {
       return Math.pow(this.RGB[0] - compare.RGB[0], 2) + Math.pow(this.RGB[1] - compare.RGB[1], 2)
           + Math.pow(this.RGB[2] - compare.RGB[2], 2);
     }
@@ -95,8 +91,8 @@ public class Spinner extends SubsystemBase {
      * @return expected next color on the wheel (clockwise) based on the index of
      *         {@link #colorMap}
      */
-    public Color nextColor() {
-      Color expected = null;
+    public ColorHelper nextColor() {
+      ColorHelper expected = null;
       for (int i = 0; i < COLOR_MAP.length; i++) {
         if (this.compareTo(COLOR_MAP[i])) {
           try {
@@ -131,8 +127,8 @@ public class Spinner extends SubsystemBase {
   /**
    * @return Color the sensor is looking at in Color format
    */
-  public Color getColor() {
-    return new Color(colorSensor.getRawColor());
+  public ColorHelper getColor() {
+    return new ColorHelper(colorSensor.getColor());
   }
 
   /**
@@ -146,7 +142,7 @@ public class Spinner extends SubsystemBase {
    * Does fact checking, should make it more rigorous.
    */
   public void runSpinner() {
-    Color currentColor = getColor();
+    ColorHelper currentColor = getColor();
     if (!currentColor.compareTo(lastColor) && currentColor.compareTo(lastColor.nextColor())) {
       lastColor = lastColor.nextColor();
       if (currentColor.compareTo(startColor))
