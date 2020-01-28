178ab4f97b7f773a8079460d95eb7e185b0db7a5
frcteam1502 <frcteam1502@gmail.com>
2020-01-27 19:40:02 -0500

    Changed brushed motors to brushless in constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,13 +29,13 @@ public final class Constants {
 
     public static final class Motors {
         public static final CANSparkMax RANDOM_MOTOR = new CANSparkMax(1, kBrushless);
-        public static final CANSparkMax DRIVE_FRONT_LEFT = new CANSparkMax(2, kBrushed);
-        public static final CANSparkMax DRIVE_BACK_LEFT = new CANSparkMax(4, kBrushed);
-        public static final CANSparkMax DRIVE_FRONT_RIGHT = new CANSparkMax(3, kBrushed);
-        public static final CANSparkMax DRIVE_BACK_RIGHT = new CANSparkMax(5, kBrushed);
-        public static final CANSparkMax INTAKE_TOP = new CANSparkMax(6, kBrushed);
-        public static final CANSparkMax INTAKE_BOTTOM = new CANSparkMax(7, kBrushed);
-        public static final CANSparkMax INTAKE_WHEEL = new CANSparkMax(8, kBrushed);
+        public static final CANSparkMax DRIVE_FRONT_LEFT = new CANSparkMax(2, kBrushless);
+        public static final CANSparkMax DRIVE_BACK_LEFT = new CANSparkMax(4, kBrushless);
+        public static final CANSparkMax DRIVE_FRONT_RIGHT = new CANSparkMax(3, kBrushless);
+        public static final CANSparkMax DRIVE_BACK_RIGHT = new CANSparkMax(5, kBrushless);
+        public static final CANSparkMax INTAKE_TOP = new CANSparkMax(6, kBrushless);
+        public static final CANSparkMax INTAKE_BOTTOM = new CANSparkMax(7, kBrushless);
+        public static final CANSparkMax INTAKE_WHEEL = new CANSparkMax(8, kBrushless);
     }
 
     public static class Sensors {
