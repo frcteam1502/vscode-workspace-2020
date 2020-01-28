7642fc66e977233f21fd0c6884930c3987cf6cd7
frcteam1502 <frcteam1502@gmail.com>
2020-01-27 19:21:28 -0500

    Color sensor stuff

--- src/main/java/frc/robot/commands/SpinnerLiftDown.java
+++ src/main/java/frc/robot/commands/SpinnerLiftDown.java
@@ -9,11 +9,11 @@ package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class SpinnerLiftGo extends CommandBase {
+public class SpinnerLiftDown extends CommandBase {
   /**
    * Creates a new SpinnerLiftGo.
    */
-  public SpinnerLiftGo() {
+  public SpinnerLiftDown() {
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
