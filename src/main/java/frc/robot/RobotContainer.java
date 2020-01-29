0c6df6d41578eae356221dd0098f9e44b1ce3036
frcteam1502 <frcteam1502@gmail.com>
2020-01-28 20:13:55 -0500

    So I can switch branches-Evan (Treevore not here)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -25,13 +25,19 @@ public class RobotContainer {
   // new CANSparkMax(Motors.DRIVE_FRONT_RIGHT, kBrushed),
   // new CANSparkMax(Motors.DRIVE_BACK_RIGHT, kBrushed));
 
-  public static final SpinnerLift spinnerLift = new SpinnerLift(Sensors.SPINNER_LOWER_LIFT_LIMIT,
-      Sensors.SPINNER_UPPER_LIFT_LIMIT, Motors.SPINNER_LIFT_MOTOR);
-  public static final SpinnerUpCommand spinnerLiftUp = new SpinnerUpCommand(spinnerLift);
-  public static final SpinnerDownCommands spinnerLiftDown = new SpinnerDownCommands(spinnerLift);
+  // public static final SpinnerLift spinnerLift = new
+  // SpinnerLift(Sensors.SPINNER_LOWER_LIFT_LIMIT,
+  // Sensors.SPINNER_UPPER_LIFT_LIMIT, Motors.SPINNER_LIFT_MOTOR);
+  // public static final SpinnerUpCommand spinnerLiftUp = new
+  // SpinnerUpCommand(spinnerLift);
+  // public static final SpinnerDownCommands spinnerLiftDown = new
+  // SpinnerDownCommands(spinnerLift);
 
-  public static final Spinner spinner = new Spinner(Sensors.COLOR_SENSOR, Motors.SPINNER_MOTOR);
-  public static final SpinCommand spinnerCommands = new SpinCommand(spinner);
+  // public static final Spinner spinner = new Spinner(Sensors.COLOR_SENSOR,
+  // Motors.SPINNER_MOTOR);
+  // public static final SpinCommand spinnerCommands = new SpinCommand(spinner);
+
+  public static final ColorTest colorTest = new ColorTest(Sensors.COLOR_SENSOR);
 
   /**
    * The container for the robot. Contains subsystems, OI devices, and commands.
@@ -48,7 +54,7 @@ public class RobotContainer {
    * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
    */
   private void configureButtonBindings() {
-    Buttons.BACK.whenPressed(spinnerLiftUp.andThen(spinnerCommands).andThen(spinnerLiftDown));
+    // Buttons.BACK.whenPressed(spinnerLiftUp.andThen(spinnerCommands).andThen(spinnerLiftDown));
   }
 
   /**
--- src/main/java/frc/robot/subsystems/ColorTest.java
+++ src/main/java/frc/robot/subsystems/ColorTest.java
@@ -9,8 +9,10 @@ package frc.robot.subsystems;
 
 import com.revrobotics.ColorSensorV3;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.commands.ColorTestCommand;
 
 public class ColorTest extends SubsystemBase {
 
@@ -26,18 +28,23 @@ public class ColorTest extends SubsystemBase {
    */
   public ColorTest(ColorSensorV3 sensor) {
     this.sensor = sensor;
+    setDefaultCommand(new ColorTestCommand(this));
   }
 
   private class ColorHelper extends Color {
 
     private double[] RGB = new double[3];
-    private String name;
+    private String name = null;
 
     public ColorHelper(Color color) {
       super(color.red, color.green, color.blue);
       double mag = color.red + color.green + color.blue;
       double[] RGB = { color.red / mag, color.green / mag, color.blue / mag };
       this.RGB = RGB;
+    }
+
+    public String getName() {
+      return name == null ? "none" : name;
     }
 
     /**
@@ -64,17 +71,26 @@ public class ColorTest extends SubsystemBase {
      */
     public ColorHelper getClosestColor() {
       ColorHelper closestColor = null;
+      int i = 0;
       for (ColorHelper wheelColor : COLOR_MAP) {
         if (closestColor == null || wheelColor.getDifference(this) < closestColor.getDifference(this)) {
           closestColor = wheelColor;
+          i++;
+          SmartDashboard.putNumber("Made it", i);
         }
       }
       return closestColor;
     }
 
     private double getDifference(ColorHelper compare) {
-      return Math.pow(this.RGB[0] - compare.RGB[0], 2) + Math.pow(this.RGB[1] - compare.RGB[1], 2)
-          + Math.pow(this.RGB[2] - compare.RGB[2], 2);
+      double comparativeRed = this.RGB[0] - compare.RGB[0];
+      double comparativeGreen = this.RGB[1] - compare.RGB[1];
+      double comparativeBlue = this.RGB[2] - compare.RGB[2];
+      SmartDashboard.putNumber("Comparative red", comparativeRed);
+      SmartDashboard.putNumber("Comparative green", comparativeGreen);
+      SmartDashboard.putNumber("Comparative blue", comparativeBlue);
+
+      return Math.pow(comparativeRed, 2) + Math.pow(comparativeGreen, 2) + Math.pow(comparativeBlue, 2);
     }
 
     /**
@@ -85,6 +101,7 @@ public class ColorTest extends SubsystemBase {
       ColorHelper expected = null;
       for (int i = 0; i < COLOR_MAP.length; i++) {
         if (this.compareTo(COLOR_MAP[i])) {
+          SmartDashboard.putNumber("Expected color index", i);
           try {
             expected = COLOR_MAP[i + 1];
           } catch (ArrayIndexOutOfBoundsException e) {
@@ -94,6 +111,23 @@ public class ColorTest extends SubsystemBase {
       }
       return expected;
     }
+  }
+
+  /**
+   * @return Color the sensor is looking at in Color format
+   */
+  public ColorHelper getColor() {
+    return new ColorHelper(sensor.getColor());
+  }
+
+  public ColorHelper getNextColorTest() {
+    ColorHelper current = getColor().getClosestColor();
+    SmartDashboard.putString("Current color", current.getName());
+    return current.nextColor();
+  }
+
+  public void run() {
+    SmartDashboard.putString("Next color", getNextColorTest().getName());
   }
 
   @Override
