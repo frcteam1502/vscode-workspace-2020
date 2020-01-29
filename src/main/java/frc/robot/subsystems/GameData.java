2472b79aafeb85c55ca698a1024e41d6cda448e4
frcteam1502 <frcteam1502@gmail.com>
2020-01-29 18:09:42 -0500

    Cant remember think it was just making game data related programs but didnt finish -Colby

---
+++ src/main/java/frc/robot/subsystems/GameData.java
@@ -0,0 +1,53 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class GameData extends SubsystemBase {
+  /**
+   * Creates a new GameData.
+   */
+
+  enum Colors {
+    Red('R'), Green('G'), Blue('B'), Yellow('Y');
+
+    char initial;
+
+    Colors(char initial) {
+      this.initial = initial;
+    }
+
+    @Override
+    public String toString() {
+      return Character.toString(initial);
+    }
+
+    public static Colors GetColor(char c) {
+      for (Colors color : Colors.values()) {
+        if (color.initial == c) {
+          return color;
+        }
+      }
+      return null;
+    }
+  }
+
+  public GameData() {
+
+  }
+
+  public void GetData() {
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
