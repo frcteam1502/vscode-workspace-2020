a36597c2b9d2968def9bb312df9ff874fdb9f1e9
frcteam1502 <frcteam1502@gmail.com>
2020-01-30 19:31:08 -0500

    Pushh

---
+++ src/main/java/frc/robot/subsystems/SkidDriveTrain.java
@@ -0,0 +1,51 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class SkidDriveTrain extends SubsystemBase {
+
+  ArrayList<CANSparkMax> leftMotors = new ArrayList<>();
+  ArrayList<CANSparkMax> rightMotors = new ArrayList<>();
+  ArrayList<CANSparkMax> motors = new ArrayList<>();
+  Joystick left, right;
+
+  public SkidDriveTrain(Joystick left, Joystick right, CANSparkMax... motors) {
+    Collections.addAll(this.motors, motors);
+    leftMotors = (ArrayList<CANSparkMax>) this.motors.subList(0, (this.motors.size() - 1) / 2);
+    rightMotors = (ArrayList<CANSparkMax>) this.motors.subList((this.motors.size() - 1) / 2, this.motors.size() - 1);
+  }
+
+  public void move() {
+    double moveSpeed = left.getY();
+    double rotateSpeed = right.getX();
+    double leftPwr = -moveSpeed + rotateSpeed;
+    double rightPwr = moveSpeed + rotateSpeed;
+    if ((leftPwr > 1 || leftPwr < -1) || (rightPwr > 1 || rightPwr < -1)) {
+      double max = Math.abs(Math.abs(leftPwr) > Math.abs(rightPwr) ? leftPwr : rightPwr);
+      leftPwr = leftPwr / max;
+      rightPwr = rightPwr / max;
+    }
+    final double fRightPower = rightPwr;
+    final double fLeftPower = leftPwr;
+    rightMotors.forEach(x -> x.set(fRightPower));
+    leftMotors.forEach(x -> x.set(fLeftPower));
+  }
+
+  @Override
+  public void periodic() {
+    move();
+  }
+}
