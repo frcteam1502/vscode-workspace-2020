b56474ce2683253e9d36b69f1618cb1fefb4bf0a
frcteam1502 <frcteam1502@gmail.com>
2020-02-01 14:36:51 -0500

    Commenting on spinner

--- src/main/java/frc/robot/subsystems/Spinner.java
+++ src/main/java/frc/robot/subsystems/Spinner.java
@@ -19,15 +19,34 @@ public class Spinner extends SubsystemBase {
     this.colorSensor = colorSensor;
   }
 
+  /**
+   * <b>Side Effects</b>
+   * <ul>
+   * <li>Sets start color as the color closest to the current color
+   * <li>Sets the last color as the start color
+   * </ul>
+   */
   public void setColor() {
     this.startColor = getColor().getClosestColor();
     this.lastColor = startColor;
   }
 
+  /**
+   * @return instance of ColorKeystone with RGB values current being looked at
+   */
   public ColorKeystone getColor() {
     return ColorKeystone.getInstance(colorSensor.getColor());
   }
 
+  /**
+   * Side effects
+   * <ul>
+   * <li>sets last color as current color if current color != last color
+   * <li>increments counter if current == start color but != to last color
+   * </ul>
+   * 
+   * @return Value of counter is less than 8
+   */
   public boolean checkSpinner() {
     ColorKeystone current = getColor().getClosestColor();
     if (current != null || current != this.lastColor) {
