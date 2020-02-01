ceb090ba98d9e5f5902a7e7f22e9b0a775e8cf44
frcteam1502 <frcteam1502@gmail.com>
2020-02-01 14:50:59 -0500

    "Fixed" Drivetrain and "Finished" Kickable

--- src/main/java/frc/robot/subsystems/Kickable.java
+++ src/main/java/frc/robot/subsystems/Kickable.java
@@ -1,14 +1,23 @@
 package frc.robot.subsystems;
 
 import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.PIDController;
 
-@Deprecated(since = "2020", forRemoval = true) // hehe
 public class Kickable extends SubsystemBase {
+
+  PIDController PID = new PIDController(0, 0, 0);
 
   private final AHRS gyro;
   private final double initial;
+
+  CANSparkMax FRONT_LEFT = new CANSparkMax(1, MotorType.kBrushless);
+  CANSparkMax BACK_LEFT = new CANSparkMax(2, MotorType.kBrushless);
+  CANSparkMax FRONT_RIGHT = new CANSparkMax(3, MotorType.kBrushless);
+  CANSparkMax BACK_RIGHT = new CANSparkMax(4, MotorType.kBrushless);
 
   public Kickable(AHRS gyro) {
     this.gyro = gyro;
@@ -16,11 +25,11 @@ public class Kickable extends SubsystemBase {
   }
 
   public void fixRotation() {
-    if (gyro.getYaw() > initial) {
-      // Drivetrain.rightPwr = PIDController.getError();
-    } else if (gyro.getYaw() < initial) {
-
-    }
+    PID.input(gyro.getYaw() - initial);
+    FRONT_RIGHT.set(PID.getCorrection());
+    FRONT_LEFT.set(PID.getCorrection());
+    BACK_RIGHT.set(PID.getCorrection());
+    BACK_RIGHT.set(PID.getCorrection());
   }
 
   @Override
