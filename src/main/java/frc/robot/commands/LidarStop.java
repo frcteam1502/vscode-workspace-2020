889139cc70fa73d72df92f9b76491f2ff4d46fe6
frcteam1502 <frcteam1502@gmail.com>
2020-02-19 18:50:59 -0500

    Correctly implimented joystick

--- src/main/java/frc/robot/commands/LidarStop.java
+++ src/main/java/frc/robot/commands/LidarStop.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 
 import java.util.function.Supplier;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants;
 import frc.robot.PIDController;
