0eed7464dd54f5722c03ad737c828c9d4864b893
frcteam1502 <frcteam1502@gmail.com>
2020-02-19 18:50:44 -0500

    Made new intake

---
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -0,0 +1,29 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Intake extends SubsystemBase {
+
+  private CANSparkMax wheel;
+
+  public Intake(CANSparkMax wheel) {
+    this.wheel = wheel;
+  }
+
+  public void run(double speed) {
+    wheel.set(speed);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
